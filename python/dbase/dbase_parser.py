fe848ded702bdf830287c100a6e376643551fd52
Azamat H. Hackimov <azamat.hackimov@gmail.com>
2017-07-01 22:00:01 +0500

    Simplify code

--- python/dbase/dbase_parser.py
+++ python/dbase/dbase_parser.py
@@ -1,5 +1,5 @@
 import os
-import construct
+from construct import *
 import argparse
 
 # Built for construct >= 2.8
@@ -9,7 +9,7 @@ import argparse
 # and Sequences.
 # Most classes were redesigned and reimplemented. You should read the
 # documentation again.
-if 2 <= construct.version[0] and 8 > construct.version[1]:
+if 2 <= version[0] and 8 > version[1]:
     raise ValueError("Built for construct >= 2.8 only")
 
 
@@ -23,68 +23,68 @@ def hexdump(src, length=16):
         lines.append("%04x  %-*s  %s\n" % (c, length*3, hex, printable))
     return ''.join(lines).rstrip('\n')
 
-dbase100_cutscene_entry = construct.Struct(
-    "name"                      / construct.String(8),
-    "unk_dword_12"              / construct.Int32ul,
-    "dbase400_cutscene_offset"  / construct.Int32ul,
-    "unk_dword_13"              / construct.Int32ul
+dbase100_cutscene_entry = Struct(
+    "name"                      / String(8),
+    "unk_dword_12"              / Int32ul,
+    "dbase400_cutscene_offset"  / Int32ul,
+    "unk_dword_13"              / Int32ul
 )
 
 # FIXME: There counts only first DBASE400 entry, but they can be more
-dbase100_inventory_entry = construct.Struct(
-    "length"                    / construct.Int16ul,
-    "unk_byte_01"               / construct.Int8ul,
-    "unk_byte_02"               / construct.Int8ul,
-    "unk_dword_01"              / construct.Int32ul,
-    "unk_dword_02"              / construct.Int32ul,
-    "unk_dword_03"              / construct.Int32ul,
-    "dbase400_inventory_offset" / construct.Int32ul,
-    "unk_dword_04"              / construct.Int32ul,
-    "unk_bytes_01"              / construct.Bytes(lambda ctx: ctx.length - 20)
+dbase100_inventory_entry = Struct(
+    "length"                    / Int16ul,
+    "unk_byte_01"               / Int8ul,
+    "unk_byte_02"               / Int8ul,
+    "unk_dword_01"              / Int32ul,
+    "unk_dword_02"              / Int32ul,
+    "unk_dword_03"              / Int32ul,
+    "dbase400_inventory_offset" / Int32ul,
+    "unk_dword_04"              / Int32ul,
+    "unk_bytes_01"              / Bytes(lambda ctx: ctx.length - 20)
 )
 
-dbase100_inventory_offset = construct.Struct(
-    "offset"                    / construct.Int32ul,
-    "entry"                     / construct.Pointer(lambda ctx: ctx.offset, dbase100_inventory_entry)
+dbase100_inventory_offset = Struct(
+    "offset"                    / Int32ul,
+    "entry"                     / Pointer(lambda ctx: ctx.offset, dbase100_inventory_entry)
 )
 
-dbase100_file = construct.Struct(
-    "signature"                 / construct.Const("DBASE100"),     # + 0x00
-    "filesize"                  / construct.Int32ul,               # + 0x08
-    "unk_dword_02"              / construct.Int32ul,               # + 0x0C
-    "nb_dbase100_inventory"     / construct.Int32ul,               # + 0x10
-    "dbase100_table_offset_inventory"   / construct.Int32ul,       # + 0x14        // offset
-    "unk_dword_05"              / construct.Int32ul,               # + 0x18
-    "ns_offset_01"              / construct.Int32ul,               # + 0x1C        // offset
-    "nb_dbase400_cutscene"      / construct.Int32ul,               # + 0x20        // nb * 0x14
-    "dbase400_table_offset_cutscene"    / construct.Int32ul,       # + 0x24        // offset
-    "nb_dbase400_interface"     / construct.Int32ul,               # + 0x28        // nb * 0x04
-    "dbase400_table_offset_interface"   / construct.Int32ul,       # + 0x2C        // offset
-    "unk_dword_11"              / construct.Int32ul,               # + 0x30
+dbase100_file = Struct(
+    "signature"                 / Const("DBASE100"),     # + 0x00
+    "filesize"                  / Int32ul,               # + 0x08
+    "unk_dword_02"              / Int32ul,               # + 0x0C
+    "nb_dbase100_inventory"     / Int32ul,               # + 0x10
+    "dbase100_table_offset_inventory"   / Int32ul,       # + 0x14        // offset
+    "unk_dword_05"              / Int32ul,               # + 0x18
+    "ns_offset_01"              / Int32ul,               # + 0x1C        // offset
+    "nb_dbase400_cutscene"      / Int32ul,               # + 0x20        // nb * 0x14
+    "dbase400_table_offset_cutscene"    / Int32ul,       # + 0x24        // offset
+    "nb_dbase400_interface"     / Int32ul,               # + 0x28        // nb * 0x04
+    "dbase400_table_offset_interface"   / Int32ul,       # + 0x2C        // offset
+    "unk_dword_11"              / Int32ul,               # + 0x30
 
-    "dbase100_offset_inventory" / construct.Pointer(lambda ctx: ctx.dbase100_table_offset_inventory, construct.Array(lambda ctx: ctx.nb_dbase100_inventory, dbase100_inventory_offset)),
-    "dbase400_offset_cutscene"  / construct.OnDemandPointer(lambda ctx: ctx.dbase400_table_offset_cutscene, construct.Array(lambda ctx: ctx.nb_dbase400_cutscene, dbase100_cutscene_entry)),
-    "dbase400_offset_interface" / construct.OnDemandPointer(lambda ctx: ctx.dbase400_table_offset_interface, construct.Array(lambda ctx: ctx.nb_dbase400_interface, construct.Int32ul)),
-    "dbase100_offset_01"        / construct.OnDemandPointer(lambda ctx: ctx.ns_offset_01, construct.Array(lambda ctx: ctx.unk_dword_05, construct.Int32ul))
+    "dbase100_offset_inventory" / Pointer(lambda ctx: ctx.dbase100_table_offset_inventory, Array(lambda ctx: ctx.nb_dbase100_inventory, dbase100_inventory_offset)),
+    "dbase400_offset_cutscene"  / OnDemandPointer(lambda ctx: ctx.dbase400_table_offset_cutscene, Array(lambda ctx: ctx.nb_dbase400_cutscene, dbase100_cutscene_entry)),
+    "dbase400_offset_interface" / OnDemandPointer(lambda ctx: ctx.dbase400_table_offset_interface, Array(lambda ctx: ctx.nb_dbase400_interface, Int32ul)),
+    "dbase100_offset_01"        / OnDemandPointer(lambda ctx: ctx.ns_offset_01, Array(lambda ctx: ctx.unk_dword_05, Int32ul))
 )
 
-dbase400_subtitle = construct.Struct(
-    "length"                / construct.Int16ul,
-    "unk_word_00"           / construct.Int16ul,        # duration?
-    "font_color"            / construct.Int8ul,
-    "string"                / construct.Aligned(4, construct.String(lambda ctx: ctx.length - 5))    # FIXME what about zero-length string?
+dbase400_subtitle = Struct(
+    "length"                / Int16ul,
+    "unk_word_00"           / Int16ul,        # duration?
+    "font_color"            / Int8ul,
+    "string"                / Aligned(4, String(lambda ctx: ctx.length - 5))    # FIXME what about zero-length string?
 )
 
-dbase400_entry = construct.Struct(
-    "unk_dword_00"          / construct.Int32ul,                        # + 0x00
-    "length"                / construct.Int16ul,                        # + 0x04
-    "font_color"            / construct.Int16ul,                        # + 0x06
-    "string"                / construct.Aligned(4, construct.String(lambda ctx: ctx.length))  # + 0x08
+dbase400_entry = Struct(
+    "unk_dword_00"          / Int32ul,                        # + 0x00
+    "length"                / Int16ul,                        # + 0x04
+    "font_color"            / Int16ul,                        # + 0x06
+    "string"                / Aligned(4, String(lambda ctx: ctx.length))  # + 0x08
 )
 
-dbase400_file = construct.Struct(
-    "signature"          / construct.Const("DBASE400"),     # + 0x00
-    "all"                / construct.Array(2, dbase400_entry)
+dbase400_file = Struct(
+    "signature"          / Const("DBASE400"),     # + 0x00
+    "all"                / Array(2, dbase400_entry)
 )
     
 if __name__ == '__main__':
@@ -139,7 +139,7 @@ if __name__ == '__main__':
 #            exit(0)
 #
 #        stream.seek(db100_offset + 0x10, 0x00)
-#        v = construct.Int32ul.parse(stream.read(4))
+#        v = Int32ul.parse(stream.read(4))
 #        db400_stream.seek(v, 0x00)
 #        db400_entry = dbase400_entry.parse_stream(db400_stream)
 #        print "db400 sec. stream: " + str(db400_entry)
